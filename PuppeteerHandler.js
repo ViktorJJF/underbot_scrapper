6dc7d2136370fa393091ff6e9f7af259ca6d38bf
ViktorJJF <vj.jimenez96@gmail.com>
2025-01-26 17:07:06 -0500

    Update: changed to headless

--- PuppeteerHandler.js
+++ PuppeteerHandler.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
 puppeteer.use(StealthPlugin());
 
@@ -15,19 +15,29 @@ class PuppeteerHandler {
     try {
       this.browser = await puppeteer.launch({
         timeout: 0,
-        headless: false
+        headless: true,
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage", // Prevents memory-related crashes
+          "--disable-accelerated-2d-canvas",
+          "--disable-gpu",
+        ],
+        xecutablePath: "/usr/bin/google-chrome-stable",
       });
-      console.log('Puppeteer browser launched successfully.');
+      console.log("Puppeteer browser launched successfully.");
       this.page = await this.browser.newPage();
     } catch (error) {
-      console.error('Error launching Puppeteer:', error);
+      console.error("Error launching Puppeteer:", error);
       throw error; // Re-throw the error to ensure it's caught upstream
     }
   }
 
   async getVersion() {
     if (!this.browser) {
-      console.error('Browser not initialized. Please call launchPuppeteer() first.');
+      console.error(
+        "Browser not initialized. Please call launchPuppeteer() first."
+      );
       return null;
     }
     return this.browser.version();
@@ -35,7 +45,9 @@ class PuppeteerHandler {
 
   async goToPage(url) {
     if (!this.page) {
-      console.error('Page not initialized. Please call launchPuppeteer() first.');
+      console.error(
+        "Page not initialized. Please call launchPuppeteer() first."
+      );
       return;
     }
     try {
@@ -50,9 +62,9 @@ class PuppeteerHandler {
     if (this.browser) {
       try {
         await this.browser.close();
-        console.log('Puppeteer browser closed.');
+        console.log("Puppeteer browser closed.");
       } catch (error) {
-        console.error('Error closing Puppeteer:', error);
+        console.error("Error closing Puppeteer:", error);
       }
     }
   }
@@ -63,9 +75,9 @@ class PuppeteerHandler {
 
   // for dorado bet
   async getMatchTimelineDelta(matchId) {
-    console.log("matchId: ",matchId);
+    console.log("matchId: ", matchId);
     if (this.isFetchingMatchTimelineDelta) {
-      console.log('Already fetching match timeline delta.');
+      console.log("Already fetching match timeline delta.");
       return;
     }
 
@@ -81,9 +93,9 @@ class PuppeteerHandler {
         resolvePromise = resolve;
       });
 
-      newPage.on('request', (request) => {
+      newPage.on("request", (request) => {
         const url = request.url();
-        if (url.includes('/match_timelinedelta')) {
+        if (url.includes("/match_timelinedelta")) {
           const urlObj = new URL(url);
           const matchTimeLineDeltaCredentials = {
             fullUrl: url,
@@ -103,28 +115,30 @@ class PuppeteerHandler {
       const pageUrl = `https://doradobet.com/deportes/partido/${matchId}`;
       await newPage.goto(pageUrl);
       // take screenshot
-      await newPage.screenshot({ path: 'timeline.png' });
+      await newPage.screenshot({ path: "timeline.png" });
       // Wait for credentials with 10 second timeout
       const timeoutPromise = new Promise((_, reject) => {
         setTimeout(() => {
-          reject(new Error('Timeout waiting for match timeline delta'));
+          reject(new Error("Timeout waiting for match timeline delta"));
         }, 10000);
       });
 
       try {
-        const credentials = await Promise.race([timelineDeltaPromise, timeoutPromise]);
-        await newPage.screenshot({ path: 'timeline2.png' });
+        const credentials = await Promise.race([
+          timelineDeltaPromise,
+          timeoutPromise,
+        ]);
+        await newPage.screenshot({ path: "timeline2.png" });
         await newPage.close();
         return credentials;
       } catch (error) {
-        console.log('No /match_timelinedelta request found within timeout.');
+        console.log("No /match_timelinedelta request found within timeout.");
         await newPage.close();
         this.isFetchingMatchTimelineDelta = false;
         return null;
       }
-
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
       if (newPage) await newPage.close();
       this.isFetchingMatchTimelineDelta = false;
       return null;
